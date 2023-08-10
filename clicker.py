4f10eaec2fb219f29fe933f33847b2e9e5439db2
Andreas <symiakand@gmail.com>
2023-08-10 17:49:01 +0300

    random wait time 1-6 min

--- clicker.py
+++ clicker.py
@@ -1,9 +1,7 @@
 import pyautogui
 import time
 import os
-
-
-MINUTES_TO_PAUSE = 10
+import random
 
 
 def convert_to_preferred_format(timestamp):
@@ -30,12 +28,13 @@ def main():
         x_close, y_close = pyautogui.position()
         print("\n\nPosition acquired! Auto clicking will run for %02d:%02d... \nPress Ctrl-C to quit.\n" % (max_hours, max_mins))
 
-        wait_time = .0
+        waiting_time = .0
+        minutes_to_wait = random.randint(1, 6)
         now = time.time()
         while True:
             old_now = now
             now = time.time()
-            wait_time += now - old_now
+            waiting_time += now - old_now
             running_time = now - start_time
             running_hours, running_minutes, running_seconds = convert_to_preferred_format(running_time)
             if running_hours >= max_hours and running_minutes >= max_mins:
@@ -44,9 +43,10 @@ def main():
                 pyautogui.click(x_close, y_close, clicks=1, button="left")  # click 'close window' button
                 exit(0)
             print(f'\rRunning time: %02d:%02d:%02d' % (running_hours, running_minutes, running_seconds), end=' ')
-            _, wait_minutes, _ = convert_to_preferred_format(wait_time)
-            if wait_minutes >= MINUTES_TO_PAUSE:
-                wait_time = .0
+            _, wait_minutes, _ = convert_to_preferred_format(waiting_time)
+            if wait_minutes >= minutes_to_wait:
+                waiting_time = .0
+                minutes_to_wait = random.randint(1, 6)
                 pyautogui.click(x, y - 60, clicks=1, button="left")  # click on window to bring it in first plane
                 pyautogui.click(x, y, clicks=1, button="left")  # click on selected position
             time.sleep(5)
